47255980620222f252230674757ba7c7728a36f8
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-12-03 13:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here!
+const list = document.getElementById("infi-list");
+
+
+function addListItems(count) {
+	const currentItemCount = list.children.length;
+	for(let i = 0; i <= count; i++) {
+		const listItems = document.createElement('li');
+		listItems.textContent = `Item  ${currentItemCount + 1}`;
+		list.appendChild(list);
+	}
+}
+
+addListItems(10);
+
+window.addEventListener('scroll' , () => {
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  // Check if user has scrolled to the bottom
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    addListItems(2); // Add 2 more items
+  }
+})
 
